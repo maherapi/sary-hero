4acfb37e2c53c9be16a446cca9a6db5e2aa28244
MHD Maher Azkoul <maher.azkoul@gmail.com>
2021-11-30 15:13:18 +0300

    fix: route cannot start with '/'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 const routes: Routes = [
   {
-    path: '/',
+    path: '',
     redirectTo: 'all-heros',
     pathMatch: 'full',
   },
